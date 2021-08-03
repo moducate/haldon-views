e978e77c83ba8ecd1e3bf74c86001b68be17b23a
Luke Carr <24438483+lukecarr@users.noreply.github.com>
2021-08-03 17:22:41 +0100

    feat(editions): added links to old editions

--- data/archive.ts
+++ data/archive.ts
@@ -15,6 +15,46 @@ export const newsletters: Newsletter[] = [
     filePath: 'june-july-2021',
     latest: false,
   },
+  {
+    name: 'April/May 2021',
+    filePath: 'april-may-2021',
+    latest: false,
+  },
+  {
+    name: 'February/March 2021',
+    filePath: 'february-march-2021',
+    latest: false,
+  },
+  {
+    name: 'December 2020/January 2021',
+    filePath: 'december-january-2020',
+    latest: false,
+  },
+  {
+    name: 'October/November 2020',
+    filePath: 'october-november-2020',
+    latest: false,
+  },
+  {
+    name: 'August/September 2020',
+    filePath: 'august-september-2020',
+    latest: false,
+  },
+  {
+    name: 'April/May 2020',
+    filePath: 'april-may-2020',
+    latest: false,
+  },
+  {
+    name: 'February/March 2020',
+    filePath: 'february-march-2020',
+    latest: false,
+  },
+  {
+    name: 'December 2019/January 2020',
+    filePath: 'december-january-2019',
+    latest: false,
+  },
 ]
 
 export const latest = (): Newsletter | undefined => newsletters.filter((x) => x.latest)[0]
